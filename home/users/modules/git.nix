e2aaae940a6d479001c8c991d232500e5394292f
nixos_u0 <mail@nixos.com>
2025-08-05 11:54:19 +0100

    Initial2

--- home/users/modules/git.nix
+++ home/users/modules/git.nix
@@ -1,13 +1,20 @@
-{congig,pkgs, ...}:
+{ config, pkgs, ... }:
+
 {
   #-----------------GIT--------------------------
   programs.git = {
     enable = true;
+
     userName = "nixos_u0";
     userEmail = "mail@nixos.com";
+
+    aliases = {
+      dotbackup = "!cd ~/.dotfiles && git add . && git commit -m \"Backup $(date '+%F %T')\" && git push";
+    };
+
     extraConfig = {
       init.defaultBranch = "main";
-      safe.directory = "$HOME/nixos_u0/dotfiles";
+      safe.directory = "/home/nixos_u0/.dotfiles";
     };
   };
 }
